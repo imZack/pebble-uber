d8337801698471d37dbbe2fa7297bd9202f3dd69
YuLun Shih <shih@yulun.me>
2014-09-16 21:48:43 +0800

    Bump to 2.1

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ var Accel = require('ui/accel');
 Accel.init();
 
 /* Variables */
-var APP_VERSION = "v2.0";
+var APP_VERSION = "v2.1";
 var isUpdating = false;
 var locationOptions = {"timeout": 15000, "maximumAge": 30000,
                        "enableHighAccuracy": true};
